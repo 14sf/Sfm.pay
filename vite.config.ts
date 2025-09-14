278abc134a7e430c6305e2a12ca2b924d6733b0a
14sf <fabrice.songa@gmail.com>
2025-09-14 17:23:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  root: '.',
-  base: './', // ✅ Ajouté
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html'
-    }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server: {
-    port: 3000,
-    open: true
-  }
-})
