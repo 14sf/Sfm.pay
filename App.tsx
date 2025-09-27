8a9627b706cfb52f09b03934eab7c2623ab5c523
14sf <fabrice.songa@gmail.com>
2025-09-27 14:57:43 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,3 +1,21 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Dashboard from './Dashboard'
+import Login from './Login'
+
 export default function App() {
-  return <h1>Hello from SFM Pay 👋</h1>
+  return (
+    <>
+      <nav style={{ padding: 12, background: '#eee' }}>
+        <Link to="/" style={{ marginRight: 10 }}>Home</Link>
+        <Link to="/dashboard" style={{ marginRight: 10 }}>Dashboard</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  )
 }
